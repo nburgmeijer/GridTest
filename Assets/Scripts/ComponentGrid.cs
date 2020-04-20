1d4dcb14cc5c5f6cbedf4f1783830a1f5955e23a
nburgmeijer <nick.burgmeijer@nhlstenden.com>
2020-04-20 17:07:13 +0200

    Initial Commit

---
+++ Assets/Scripts/ComponentGrid.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComponentGrid : MonoBehaviour
+{
+    private Grid _grid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _grid = new Grid(10, 10, 16);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid
+{
+    [SerializeField]
+    private int _x;
+    [SerializeField]
+    private int _y;
+    [SerializeField]
+    private int _size;
+
+    private int[,] _gridArray;
+
+    public Grid(int x, int y, int size)
+    {
+        _x = x;
+        _y = y;
+        _size = size;
+        _gridArray = new int[x, y];
+
+    }
+}
+
